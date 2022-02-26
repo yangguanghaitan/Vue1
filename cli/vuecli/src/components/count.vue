3156ab48d6e3c8c59b38cc8dca715b10a57f75b6
yangguanghaitan <2898666969@qq.com>
2022-02-27 00:11:10 +0800

    vuex: state访问状态对象

--- cli/vuecli/src/components/count.vue
+++ cli/vuecli/src/components/count.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <h2>{{msg}}</h2>
-        <hr/>
-        <h3>{{$store.state.count}}=={{$store.state.num}}=={{$store.state.count11}}</h3>
+        <hr />
+        <h3>第1种访问值的方式:{{$store.state.count}}=={{$store.state.num}}</h3>
+        <h3>第2种访问值的方式:{{count}}</h3>
         <div>
             <button @click="$store.commit('add')">+</button>
             <button @click="$store.commit('reduce')">-</button>
@@ -12,17 +13,27 @@
 
 <script>
     import store from '@/vuex/store'
+    import { mapState } from 'vuex';
     export default {
         name: '',
         data() {
             return {
-                msg:'Hello Vuex',
+                msg: 'Hello Vuex',
             }
         },
         store,
-        computed: {
-
-        },
+        //第2.1种
+        // computed: {
+        //     count() {
+        //         return this.$store.state.count+this.$store.state.num;
+        //     }
+        // },
+        //第2.2种
+        // computed:mapState(["count"]),
+        //第2.3种
+        computed: mapState({
+            count: state => state.count
+        }),
         methods: {
 
         },
