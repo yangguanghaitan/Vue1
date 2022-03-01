dfddf0eeeb411cc1d824e587809d67a77bcbf2f4
yangguanghaitan <2898666969@qq.com>
2022-03-01 23:27:35 +0800

    基础改造项目

---
+++ tem/src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+ {{message}}
+ <i class="icon iconfont  icon-canting1"></i>
+ </div>
+</template>
+
+<script>
+ export default {
+   name:'Pos',
+   data () {
+     return {
+        message:" Hello Pos Demo!"
+     }
+   },
+   computed:{
+ 
+   },
+   methods:{
+ 
+   },
+   components: {
+ 
+   },
+ }
+</script>
+
+<style scoped>
+ 
+</style>
