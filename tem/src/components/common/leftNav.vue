a57790460f0de8c77fc6f680ddf8890285cf9ff9
yangguanghaitan <2898666969@qq.com>
2022-03-01 23:58:14 +0800

    编写独立的侧边栏

---
+++ tem/src/components/common/leftNav.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="left-nav">
+    <ul>
+      <li>
+        <i class="icon iconfont icon-shouyintai"></i>
+        <div>收银</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-dianpu"></i>
+        <div>店铺</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-shipinleimu"></i>
+        <div>商品</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-huiyuan"></i>
+        <div>会员</div>
+      </li>
+      <li>
+        <i class="icon iconfont icon-tongjifenxi-xiangmubiaogetongji"></i>
+        <div>统计</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "leftNav(独立的侧边栏)",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style scoped>
+.left-nav {
+  color: #fff;
+  font-size: 10px;
+  height: 100%;
+  background-color: #1d8ce0;
+  float: left;
+  width: 5%;
+}
+.iconfont {
+  font-size: 24px;
+}
+.left-nav ul {
+  padding: 0px;
+  margin: 0px;
+}
+.left-nav li {
+  list-style: none;
+  text-align: center;
+  border-bottom: 1px solid #20a0ff;
+  padding: 10px;
+}
+</style>
