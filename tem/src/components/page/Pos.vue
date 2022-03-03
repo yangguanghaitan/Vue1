c3c407eda47fbfe6513fc1f258abc0f2353edc93
yangguanghaitan <2898666969@qq.com>
2022-03-03 13:57:35 +0800

    右侧框架完成

--- tem/src/components/page/Pos.vue
+++ tem/src/components/page/Pos.vue
@@ -19,7 +19,7 @@
               <el-table-column prop="price" label="金额" width="70">
               </el-table-column>
               <el-table-column label="操作" width="100" fixed="right">
-                <template scope="scope">
+                <template slot-scope="scope">
                   <el-button type="text" size="small" @click="del">删除</el-button>
                   <el-button type="text" size="small" @click="add">增加</el-button>
                 </template>
@@ -37,13 +37,46 @@
           <el-tab-pane label="外卖" name="third">
             3.外卖</el-tab-pane>
         </el-tabs>
-
-        
-
       </el-col>
+
       <!-- 右侧框架布局 -->
       <el-col :span="17">
-        111111
+        <div class="often-goods">
+          <div class="title">常用商品</div>
+          <div class="often-goods-list">
+            <ul>
+              <li v-for="good in oftenGoods">
+                <!-- <p> -->
+                <span>{{good.goodsName}}</span>
+                <span class="o-price">￥{{good.price}}元</span>
+                <!-- </p> -->
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="goods-type">
+          <el-tabs type="border-card">
+            <el-tab-pane label="汉堡">
+              <ul class='cookList'>
+                <li>
+                  <span class="foodImg"><img src="../../assets/logo.png" width="100%"></span>
+                  <span class="foodName">香辣鸡翅</span>
+                  <span class="foodPrice">￥22222222元</span>
+                </li>
+                <li v-for="good in type0Goods">
+                  <span class="foodImg"><img :src="good.goodsImg" width="100%"></span>
+                  <span class="foodName">{{good.goodsName}}</span>
+                  <span class="foodPrice">￥{{good.price}}元</span>
+                </li>
+              </ul>
+            </el-tab-pane>
+            <el-tab-pane label="小食">配置管理</el-tab-pane>
+            <el-tab-pane label="饮料">角色管理</el-tab-pane>
+            <el-tab-pane label="套餐">套餐</el-tab-pane>
+          </el-tabs>
+        </div>
+
       </el-col>
     </el-row>
   </div>
@@ -56,6 +89,7 @@
       return {
         message: " Hello Pos Demo!",
         activeName: 'third',
+
         tableData: [{
           goodsName: '可口可乐',
           price: 8,
@@ -76,6 +110,7 @@
           price: 8,
           count: 1
         }],
+
         oftenGoods: [
           {
             goodsId: 1,
@@ -118,8 +153,62 @@
             goodsName: '香脆鸡柳',
             price: 17
           }
+        ],
 
-        ]
+        type0Goods: [
+          {
+            goodsId: 1,
+            goodsImg: "http://g.hiphotos.baidu.com/image/pic/item/6d81800a19d8bc3e770bd00d868ba61ea9d345f2.jpg",
+            goodsName: '香辣鸡腿堡',
+            price: 18
+          }, {
+            goodsId: 2,
+            goodsImg: "http://b.hiphotos.baidu.com/image/pic/item/e824b899a9014c08878b2c4c0e7b02087af4f4a3.jpg",
+            goodsName: '田园鸡腿堡',
+            price: 15
+          }, {
+            goodsId: 3,
+            goodsImg: "http://c.hiphotos.baidu.com/image/pic/item/30adcbef76094b36de8a2fe5a1cc7cd98d109d99.jpg",
+            goodsName: '和风汉堡',
+            price: 15
+          }, {
+            goodsId: 4,
+            goodsImg: "http://e.hiphotos.baidu.com/image/pic/item/4bed2e738bd4b31c1badd5a685d6277f9e2ff81e.jpg",
+            goodsName: '快乐全家桶',
+            price: 80
+          }, {
+            goodsId: 5,
+            goodsImg: "http://g.hiphotos.baidu.com/image/pic/item/0d338744ebf81a4c87a3add4d52a6059252da61e.jpg",
+            goodsName: '脆皮炸鸡腿',
+            price: 10
+          }, {
+            goodsId: 6,
+            goodsImg: "http://a.hiphotos.baidu.com/image/pic/item/f2deb48f8c5494ee5080c8142ff5e0fe99257e19.jpg",
+            goodsName: '魔法鸡块',
+            price: 20
+          }, {
+            goodsId: 7,
+            goodsImg: "http://f.hiphotos.baidu.com/image/pic/item/4034970a304e251f503521f5a586c9177e3e53f9.jpg",
+            goodsName: '可乐大杯',
+            price: 10
+          }, {
+            goodsId: 8,
+            goodsImg: "http://a.hiphotos.baidu.com/image/pic/item/e824b899a9014c087eb617650e7b02087af4f464.jpg",
+            goodsName: '雪顶咖啡',
+            price: 18
+          }, {
+            goodsId: 9,
+            goodsImg: "http://d.hiphotos.baidu.com/image/pic/item/b58f8c5494eef01f119945cbe2fe9925bc317d2a.jpg",
+            goodsName: '大块鸡米花',
+            price: 15
+          }, {
+            goodsId: 20,
+            goodsImg: "http://a.hiphotos.baidu.com/image/pic/item/8d5494eef01f3a292d2472199d25bc315d607c7c.jpg",
+            goodsName: '香脆鸡柳',
+            price: 17
+          }
+
+        ],
       }
     },
     computed: {
@@ -157,8 +246,8 @@
 
   .title {
     height: 20px;
-    border-bottom: 1px solid #D3DCE6;
-    background-color: #F9FAFC;
+    border-bottom: 5px solid #D3DCE6;
+    background-color: #D3DCE6;
     padding: 10px;
   }
 
@@ -170,10 +259,53 @@
     margin: 5px;
     background-color: #fff;
   }
+
   .o-price {
     color: #58B7FF;
   }
-  .buttonInfo{
+
+  .buttonInfo {
     margin-top: 10px;
+  }
+
+  .goods-type {
+    /* 清除浮动 */
+    clear: both;
+  }
+
+  .cookList li {
+    list-style: none;
+    width: 23%;
+    border: 1px solid #E5E9F2;
+    height: auot;
+    overflow: hidden;
+    background-color: #fff;
+    padding: 2px;
+    float: left;
+    margin: 2px;
+
+  }
+
+  .cookList li span {
+
+    display: block;
+    float: left;
+  }
+
+  .foodImg {
+    width: 40%;
+  }
+
+  .foodName {
+    font-size: 18px;
+    padding-left: 10px;
+    color: brown;
+
+  }
+
+  .foodPrice {
+    font-size: 16px;
+    padding-left: 10px;
+    padding-top: 10px;
   }
 </style>
