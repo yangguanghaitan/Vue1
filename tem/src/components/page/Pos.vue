8a105d50ce52c85ff47253a8541a955451055bdf
yangguanghaitan <2898666969@qq.com>
2022-03-03 12:26:43 +0800

    左侧框架完成

--- tem/src/components/page/Pos.vue
+++ tem/src/components/page/Pos.vue
@@ -4,11 +4,46 @@
     <i class="icon iconfont  icon-canting1"></i> -->
 
     <el-row>
+      <!-- 左侧框架 -->
       <el-col v-bind:span="7" class="pos-order" id="order-list">
-        商品分类栏
+        <!--Tabs标签页  -->
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+
+          <el-tab-pane label="点餐" name="first">
+            <!-- Table表单 -->
+            <el-table :data="tableData" border show-summary style="width: 100%">
+              <el-table-column prop="goodsName" label="商品" width="100">
+              </el-table-column>
+              <el-table-column prop="count" label="量" width="50">
+              </el-table-column>
+              <el-table-column prop="price" label="金额" width="70">
+              </el-table-column>
+              <el-table-column label="操作" width="100" fixed="right">
+                <template scope="scope">
+                  <el-button type="text" size="small" @click="del">删除</el-button>
+                  <el-button type="text" size="small" @click="add">增加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="buttonInfo">
+              <el-button type="success">结账</el-button>
+              <el-button type="info">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+            </div>
+          </el-tab-pane>
+
+          <el-tab-pane label="挂单" name="second">
+            2.挂单</el-tab-pane>
+          <el-tab-pane label="外卖" name="third">
+            3.外卖</el-tab-pane>
+        </el-tabs>
+
+        
+
       </el-col>
+      <!-- 右侧框架布局 -->
       <el-col :span="17">
-        商品展示栏
+        111111
       </el-col>
     </el-row>
   </div>
@@ -19,31 +54,126 @@
     name: 'Pos',
     data() {
       return {
-        message: " Hello Pos Demo!"
+        message: " Hello Pos Demo!",
+        activeName: 'third',
+        tableData: [{
+          goodsName: '可口可乐',
+          price: 8,
+          count: 1
+        }, {
+
+          goodsName: '香辣鸡腿堡',
+          price: "15",
+          count: 1
+        }, {
+
+          goodsName: '爱心薯条',
+          price: 8,
+          count: 1
+        }, {
+
+          goodsName: '甜筒',
+          price: 8,
+          count: 1
+        }],
+        oftenGoods: [
+          {
+            goodsId: 1,
+            goodsName: '香辣鸡腿堡',
+            price: 18
+          }, {
+            goodsId: 2,
+            goodsName: '田园鸡腿堡',
+            price: 15
+          }, {
+            goodsId: 3,
+            goodsName: '和风汉堡',
+            price: 15
+          }, {
+            goodsId: 4,
+            goodsName: '快乐全家桶',
+            price: 80
+          }, {
+            goodsId: 5,
+            goodsName: '脆皮炸鸡腿',
+            price: 10
+          }, {
+            goodsId: 6,
+            goodsName: '魔法鸡块',
+            price: 20
+          }, {
+            goodsId: 7,
+            goodsName: '可乐大杯',
+            price: 10
+          }, {
+            goodsId: 8,
+            goodsName: '雪顶咖啡',
+            price: 18
+          }, {
+            goodsId: 9,
+            goodsName: '大块鸡米花',
+            price: 15
+          }, {
+            goodsId: 20,
+            goodsName: '香脆鸡柳',
+            price: 17
+          }
+
+        ]
       }
     },
     computed: {
 
     },
     methods: {
-
+      handleClick(tab, event) {
+        console.log(tab, event);
+      },
+      del() {
+        alert(111)
+      },
+      add() {
+        alert(222)
+      }
     },
     components: {
 
     },
     mounted: function () {
-      var orderHeight=document.body.clientHeight;
-      console.log("orderHeight:"+orderHeight);
-      document.getElementById("order-list").style.height=orderHeight+'px';
+      var orderHeight = document.body.clientHeight;
+      // console.log("orderHeight:" + orderHeight);
+      document.getElementById("order-list").style.height = orderHeight + 'px';
     },
   }
 </script>
 
 <style scoped>
- .pos-order{
-  background-color: #c4c7c9;
-  border-right: 1px double #080808;
-  text-align: center;
-  height: 100%;
-}
+  .pos-order {
+    background-color: #c4c7c9;
+    border-right: 1px double #080808;
+    text-align: center;
+    height: 100%;
+  }
+
+  .title {
+    height: 20px;
+    border-bottom: 1px solid #D3DCE6;
+    background-color: #F9FAFC;
+    padding: 10px;
+  }
+
+  .often-goods-list ul li {
+    list-style: none;
+    float: left;
+    border: 1px solid #E5E9F2;
+    padding: 10px;
+    margin: 5px;
+    background-color: #fff;
+  }
+  .o-price {
+    color: #58B7FF;
+  }
+  .buttonInfo{
+    margin-top: 10px;
+  }
 </style>
